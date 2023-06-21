b664297b3445d625c927010eef45b706464b22ac
zhicheng00 <zhichengmoo@gmail.com>
2023-06-21 13:48:47 +0800

    update sample output for parsing
    sample output from disk quota status command

--- test/test_command.rb
+++ test/test_command.rb
@@ -5,20 +5,34 @@ class TestCommand < Minitest::Test
 
   def test_command_output_parsing
     output = <<-EOF
+  Reported at: Fri Jun 16 11:00:30 +08 2023 
+ -----------------------------------------------------------------
+  Disk space for HPC home directory
+ -----------------------------------------------------------------
+     HPC Home Dir        Usage      Quota      Limit    Status
+  /home/svu/e0775105    1.57GB,      20GB,      20GB,   7.8%
+ =================================================================
 
-efranz    30328  0.1  0.1 462148 28128 ?        Sl   20:28   0:00 Passenger RackApp: /users/PZS0562/efranz/awesim/dev/ood-example-ps
-
+ -----------------------------------------------------------------
+  Disk space for HPC WorkSpace
+ -----------------------------------------------------------------
+     HPC WorkSpace            Usage,            Limit,  Status
+  /hpctmp/e0775105             0.00,          500.00G,   0.00%
+ =================================================================
 EOF
-    processes = Command.new.parse(output)
+    quotas = Command.new.parse(output)
 
-    assert_equal 1, processes.count
+    assert_equal 1, quotas.count, "number of structs parsed should equal 1"
 
-    p = processes.first
+    q = quotas.first
 
-    assert_equal "efranz", p.user
-    assert_equal "462148", p.vsz
-    assert_equal "28128", p.rss
-    assert_equal "0:00", p.time
-    assert_equal "Passenger RackApp: /users/PZS0562/efranz/awesim/dev/ood-example-ps", p.command
+    assert_equal "/home/svu/e0775105", q.homedir, "expected homedir value not correct"
+    assert_equal "1.57GB,", q.home_usage, "expected home_usage value not correct"
+    assert_equal "20GB,", q.home_limit, "expected home_limit value not correct"
+    assert_equal "7.8%", q.home_status, "expected home_status value not correct"
+    assert_equal "/hpctmp/e0775105", q.hpctmpdir, "expected hpctmpdir value not correct"
+    assert_equal "0.00,", q.hpctmp_usage, "expected hpctmp_usage value not correct"
+    assert_equal "500.00G,", q.hpctmp_limit, "expected hpctmp_limit value not correct"
+    assert_equal "0.00%", q.hpctmp_status, "expected hpctmp_status value not correct"
   end
 end
